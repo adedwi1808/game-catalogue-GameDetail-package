224308a084860648ff700ff3e8a0adc8385e4694
Ade Dwi Prayitno <adedwip1808@gmail.com>
2026-01-16 23:24:54 +0700

    Sync dependency versions

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
 .package(url: "https://github.com/adedwi1808/game-catalogue-Common-package.git", from: "1.0.0"),
         .package(
             url: "https://github.com/onevcat/Kingfisher.git",
-            from: "8.0.0"
+            from: "8.6.2"
         ),
     ],
     targets: [
